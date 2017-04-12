fd2d5c7da5a2b41d40652c03e36ab46a1bad0ee1
Kasidit Iamthong <kasidit@outlook.com>
2017-04-12 11:45:42 +0700

    Add Chart.js and Moment.js samples

--- routes/homepage.js
+++ routes/homepage.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const moment = require('moment');
 const router = express.Router();
 
 router.get('/', function(req, res) {
-  res.render('homepage', { title: 'Express' });
+  res.render('homepage', {
+    serverTime: moment().format('LLLL')
+  });
 });
 
 module.exports = router;
