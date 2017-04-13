05083fd4e42e0e5ef8af9681d93c486cdacf140c
ChanatipSaetia <chanatippung@gmail.com>
2017-04-14 01:44:19 +0700

    add front enroll

--- routes.js
+++ routes.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const homepageRoute = require('./routes/homepage');
 const studentInfoRoute = require('./routes/student_info');
+const enrollRoute = require('./routes/enroll');
 
 const router = express.Router();
 
 router.use('/', homepageRoute);
 router.use('/student-info', studentInfoRoute);
+router.use('/enroll', enrollRoute);
 
 module.exports = router;
