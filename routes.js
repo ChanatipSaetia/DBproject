d0dd33049b02f0328017a90515977650a0b642cd
zFord Nutthawat <zfordn@gmail.com>
2017-04-16 01:34:42 +0700

    Add Advisor Tab

--- routes.js
+++ routes.js
@@ -3,11 +3,13 @@ const express = require('express');
 const homepageRoute = require('./routes/homepage');
 const studentInfoRoute = require('./routes/student_info');
 const enrollRoute = require('./routes/enroll');
+const advisorRoute = require('./routes/advisor');
 
 const router = express.Router();
 
 router.use('/', homepageRoute);
 router.use('/student-info', studentInfoRoute);
 router.use('/enroll', enrollRoute);
+router.use('/advisor', advisorRoute);
 
 module.exports = router;
