37f169693ee01d8abca75e6d36d96cc8bf62eebf
Kasidit Iamthong <kasidit@outlook.com>
2017-04-18 05:23:33 +0700

    Add major and student data

--- tools/dbseed/schema/department.js
+++ tools/dbseed/schema/department.js
@@ -7,6 +7,13 @@ class DepartmentTable extends BaseTable {
 
   ensureMockData() {
     super.ensureMockData();
+
+    const departmentMapper = {
+      '21': ['วิศวกรรม', 'Engineering'],
+      '22': ['อักษรศาสตร์เอก', 'Arts'],
+      '23': ['วิทยาศาสตร์', 'Science'],
+      '25': ['สถาปัตยกรรม', 'Architecture']
+    };
     
     const departmentKeywords = [
       ['คอมพิวเตอร์', 'Computer'],
@@ -19,12 +26,12 @@ class DepartmentTable extends BaseTable {
     const faculties = super.getDepTable('faculty').getData();
     for (let i = 0; i < faculties.length; i++) {
       const faculty = faculties[i];
-      const shortFacultyNameTH = faculty.name_th.slice(3);
-      const shortFacultyNameEN = faculty.name_en.substring(11);
+      const shortFacultyNameTH = departmentMapper[faculty.fid][0];
+      const shortFacultyNameEN = departmentMapper[faculty.fid][1];
       for (let j = 0; j < Math.max(2, departmentKeywords.length - i); j++) {
         const keyword = departmentKeywords[j];
         super.putData({
-          did: i * 100 + j,
+          did: i * 100 + j + 1,
           name_th: shortFacultyNameTH + keyword[0],
           name_en: keyword[1] + ' ' + shortFacultyNameEN,
           fid: faculty.fid
--- tools/dbseed/schema/major.js
+++ tools/dbseed/schema/major.js
@@ -14,6 +14,47 @@ class MajorTable extends BaseTable {
 
   ensureMockData() {
     super.ensureMockData();
+
+    const genRequiredNumber = (fullNumber) => {
+      if (Math.random() > 0.2) {
+        return fullNumber;
+      }
+      return 0;
+    };    
+
+    const departments = super.getDepTable('department').getData();
+    let idCounter = 1;
+    for (const department of departments) {
+      if (department.name_en.includes('Electrical')) {
+        super.putData({
+          mid: idCounter,
+          name_en: 'Communication ' + department.name_en,
+          name_th: department.name_th + 'สื่อสาร',
+          did: department.did,
+          required_lang: genRequiredNumber(3),
+          required_approve: genRequiredNumber(18)
+        })
+        super.putData({
+          mid: idCounter + 1,
+          name_en: 'Power ' + department.name_en,
+          name_th: department.name_th + 'กำลัง',
+          did: department.did,
+          required_lang: genRequiredNumber(3),
+          required_approve: genRequiredNumber(18)
+        })
+        idCounter += 2;
+      } else {
+        super.putData({
+          mid: idCounter,
+          name_en: department.name_en,
+          name_th: department.name_th,
+          did: department.did,
+          required_lang: genRequiredNumber(6),
+          required_approve: genRequiredNumber(15)
+        })
+        idCounter++;
+      }
+    }
   }
 }
 
