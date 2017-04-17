e290bc5b72253bd960f0ac62c51ba2ca5089b13e
Kasidit Iamthong <kasidit@outlook.com>
2017-04-18 03:42:46 +0700

    Add department data

--- tools/dbseed/schema/department.js
+++ tools/dbseed/schema/department.js
@@ -7,6 +7,30 @@ class DepartmentTable extends BaseTable {
 
   ensureMockData() {
     super.ensureMockData();
+    
+    const departmentKeywords = [
+      ['คอมพิวเตอร์', 'Computer'],
+      ['ไฟฟ้า', 'Electrical'],
+      ['โยธา', 'Civil'],
+      ['นิวเคลียร์', 'Nuclear'],
+      ['เคมี', 'Chemical']
+    ];
+
+    const faculties = super.getDepTable('faculty').getData();
+    for (let i = 0; i < faculties.length; i++) {
+      const faculty = faculties[i];
+      const shortFacultyNameTH = faculty.name_th.slice(3);
+      const shortFacultyNameEN = faculty.name_en.substring(11);
+      for (let j = 0; j < Math.max(2, departmentKeywords.length - i); j++) {
+        const keyword = departmentKeywords[j];
+        super.putData({
+          did: i * 100 + j,
+          name_th: shortFacultyNameTH + keyword[0],
+          name_en: keyword[1] + ' ' + shortFacultyNameEN,
+          fid: faculty.fid
+        });
+      }
+    }
   }
 }
 
--- tools/dbseed/schema/faculty.js
+++ tools/dbseed/schema/faculty.js
@@ -8,25 +8,10 @@ class FacultyTable extends BaseTable {
   ensureMockData() {
     super.ensureMockData();
     const facultyData = [
-      ['21', 'คณะวิศวกรรมศาสตร์', 'Faculty Of Engineering'],
-      ['22', 'คณะอักษรศาสตร์', 'Faculty Of Arts'],
-      ['23', 'คณะวิทยาศาสตร์', 'Faculty Of Science'],
-      ['24', 'คณะรัฐศาสตร์', 'Faculty Of Political Science'],
-      ['25', 'คณะสถาปัตยกรรมศาสตร์', 'Faculty Of Architecture'],
-      ['26', 'คณะพาณิชยศาสตร์และการบัญชี', 'Faculty Of Commerce And Accountancy'],
-      ['27', 'คณะครุศาสตร์', 'Faculty Of Education'],
-      ['28', 'คณะนิเทศศาสตร์', 'Faculty Of Communication Arts'],
-      ['29', 'คณะเศรษฐศาสตร์', 'Faculty Of Economics'],
-      ['30', 'คณะแพทยศาสตร์', 'Faculty Of Medicine'],
-      ['31', 'คณะสัตวแพทยศาสตร์', 'Faculty Of Veterinary Science'],
-      ['32', 'คณะทันตแพทยศาสตร์', 'Faculty Of Dentistry'],
-      ['33', 'คณะเภสัชศาสตร์', 'Faculty Of Pharmaceutical Sciences'],
-      ['34', 'คณะนิติศาสตร์', 'Faculty Of Law'],
-      ['35', 'คณะศิลปกรรมศาสตร์', 'Faculty Of Fine And Applied Arts'],
-      ['36', 'คณะพยาบาลศาสตร์', 'Faculty Of Nursing'],
-      ['37', 'คณะสหเวชศาสตร์', 'Faculty Of Allied Health Sciences'],
-      ['38', 'คณะจิตวิทยา', 'Faculty Of Psychology'],
-      ['39', 'คณะวิทยาศาสตร์การกีฬา', 'Faculty Of Sports Science']
+      ['21', 'คณะวิศวกรรมศาสตร์', 'Faculty of Engineering'],
+      ['22', 'คณะอักษรศาสตร์', 'Faculty of Arts'],
+      ['23', 'คณะวิทยาศาสตร์', 'Faculty of Science'],
+      ['25', 'คณะสถาปัตยกรรมศาสตร์', 'Faculty of Architecture']
     ]
 
     for (const faculty of facultyData) {
