971895033513b5994fbac8c195fb1eafe8917376
Mickke <mikaecat@gmail.com>
2017-04-17 23:09:47 +0700

    Add more student search features

--- routes/student_info.js
+++ routes/student_info.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const moment = require('moment');
 const db = require('../db');
 
 const router = express.Router();
@@ -7,10 +8,35 @@ router.get('/', function (req, res, next) {
   const studentID = req.query.sid;
   const studentFirstName = req.query.sfname;
   const studentLastName = req.query.slname;
+  const studentYear = req.query.student_year;
+  const scoreConstrain = req.query.score_constrain;
+  const studentBehaveScore = req.query.behave_score;
+  const studentMajorID = req.query.mid;
+  const studentAddress = req.query.address_en;
+  const currentYear = moment().year();
 
-   if ((studentID && studentID.length > 0) || (studentFirstName && studentFirstName.length > 0) || (studentLastName && studentLastName.length > 0)) {
-    let sql = "SELECT * FROM `student` WHERE `sid` LIKE ? AND `fname_en` LIKE ? AND `lname_en` LIKE ?";
-    let inserts = ['%'+studentID.trim()+'%', '%'+studentFirstName.trim()+'%', '%'+studentLastName.trim()+'%'];
+   if ((studentID && studentID.length > 0) || (studentFirstName && studentFirstName.length > 0) || (studentLastName && studentLastName.length > 0) ||
+       (studentYear && studentYear.length > 0) || (studentBehaveScore && studentBehaveScore.length > 0 && scoreConstrain != "") || (studentMajorID && studentMajorID.length > 0) ||
+       (studentAddress && studentAddress.length > 0)) {
+      // all like
+      let sql = "SELECT * FROM student WHERE sid LIKE ? AND fname_en LIKE ? AND lname_en LIKE ? AND address_en LIKE ? ";
+      let inserts = ['%'+studentID.trim()+'%', '%'+studentFirstName.trim()+'%', '%'+studentLastName.trim()+'%', '%'+studentAddress+'%'];
+    if (studentYear && studentYear.length > 0){
+        sql += "AND ent_year = ? ";
+        inserts.push(currentYear - studentYear + 1);
+      }
+    if (studentBehaveScore && studentBehaveScore.length > 0 && scoreConstrain != ""){
+        if(scoreConstrain == ">") sql += "AND behav_score > ? ";
+        else if(scoreConstrain == "<") sql += "AND behav_score < ? ";
+        else if(scoreConstrain == "=") sql += "AND behav_score = ? ";
+
+        inserts.push(studentBehaveScore);
+    }
+    if (studentMajorID && studentMajorID.length > 0){
+      sql += "AND mid = ? ";
+      inserts.push(studentMajorID);
+    }
+
     db.query(sql, inserts,
       (err, rows) => {
         if (err) {
@@ -23,7 +49,12 @@ router.get('/', function (req, res, next) {
           sid: studentID,
           sfname: studentFirstName,
           slname: studentLastName,
-          data: rows
+          syear: studentYear,
+          behav_score: studentBehaveScore,
+          mid: studentMajorID,
+          address: studentAddress,
+          data: rows,
+          moment: moment
         });
       }
     );
