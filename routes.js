37878ba416ea8b0f7627259982766d80b4a94602
魂火SoulFire <SoulFire>
2017-04-17 23:26:01 +0700

    add login

--- routes.js
+++ routes.js
@@ -5,6 +5,7 @@ const studentInfoRoute = require('./routes/student_info');
 const managerBoardRoute = require('./routes/manager_board');
 const enrollRoute = require('./routes/enroll');
 const advisorRoute = require('./routes/advisor');
+const loginRoute = require('./routes/login');
 
 const router = express.Router();
 
@@ -12,6 +13,6 @@ router.use('/', homepageRoute);
 router.use('/student-info', studentInfoRoute);
 router.use('/manager-board', managerBoardRoute);
 router.use('/enroll', enrollRoute);
-router.use('/advisor', advisorRoute);
+router.use('/login', loginRoute);
 
 module.exports = router;
