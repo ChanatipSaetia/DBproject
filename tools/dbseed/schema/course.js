9d2b94bc607e2a9b6a96d22f5e0e70d6e8d9ea12
魂火SoulFire <chanatippung@gmail.com>
2017-04-27 13:11:01 +0700

    add course mocking

--- tools/dbseed/schema/course.js
+++ tools/dbseed/schema/course.js
@@ -1,4 +1,5 @@
 const BaseTable = require('./base-table');
+const chance = require('chance')(Math.random);
 
 class CourseTable extends BaseTable {
   constructor() {
@@ -18,8 +19,15 @@ class CourseTable extends BaseTable {
 
   ensureMockData() {
     super.ensureMockData();
+    const courseList = [...require('../course.json')];
+    const departmentData = super.getDepTable('department').getData();
+    for (let i = 0; i < courseList.length; i++) {
+      let did = departmentData[chance.integer({min: 0, max: departmentData.length - 1})].did
+      courseList[i]['did'] = did
+      console.log(courseList[i]);
+      super.putData(courseList[i])
+    }
   }
 }
 
 module.exports = CourseTable;
-
