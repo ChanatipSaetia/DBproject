3f49d1cee141da90a3013638cf4ae89db6697c6b
James Rakratchatakul <james.rak@outlook.co.th>
2017-04-30 16:36:17 +0700

    multi-query

--- routes/course.js
+++ routes/course.js
@@ -3,9 +3,28 @@ const moment = require('moment');
 const router = express.Router();
 
 router.get('/', function(req, res) {
-  res.render('course', {
-    serverTime: moment().format('LLLL')
-  });
+  const courseID = req.query.course_no;
+  const courseName = req.query.shortname;
+  
+  if(courseID && courseID.length > 0 || courseName && courseName.length > 0){
+    let sql = "SELECT * FROM course WHERE course_no LIKE ? AND shortname LIKE ? ";
+    let inserts = ['%' + courseID.trim() + '%', '%' + courseName.trim() + '%'];
+    db.query(sql, inserts,
+      (err, rows) => {
+        if (err) {
+          return next(err);
+        }
+        console.log(rows);
+        res.render('course', {
+          searched: true,
+          moment: moment,
+          serverTime: moment().format('LLLL')
+        });
+      }
+    );
+  } else {
+    res.render('course', { searched: false });
+  }
 });
 
 module.exports = router;
