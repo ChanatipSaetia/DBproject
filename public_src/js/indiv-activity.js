7a921f49173cd71bf88d93b58a5ecf07ac07aa84
Mickke <mikaecat@gmail.com>
2017-04-30 20:34:56 +0700

    Implement Activity Chart

--- public_src/js/indiv-activity.js
+++ public_src/js/indiv-activity.js
@@ -1,37 +1,53 @@
 function renderBarChart() {
   var ctx = document.getElementById('chart');
+  let labelList = [];
+  let dataList = [];
+  for(let i=0; i<data.length; i++){
+    labelList.push(data[i].year);
+    dataList.push(data[i].sum);
+  }
+
   return new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: ['2013', '2014', '2015', '2016'],
-      datasets: [{
-        label: 'จำนวนชั่วโมง',
-        data: [250,600,650,800],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.5)',
-          'rgba(54, 162, 235, 0.5)',
-          'rgba(255, 206, 86, 0.5)',
-          'rgba(75, 192, 192, 0.5)'
-        ],
-        borderColor: [
-          'rgba(255,99,132,1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)'
-        ],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: true
-          }
+      type: 'bar',
+      data: {
+        labels: labelList,
+        datasets: [{
+          label: 'จำนวนชั่วโมงรวมต่อปี',
+          data: dataList,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.5)',
+            'rgba(54, 162, 235, 0.5)',
+            'rgba(255, 206, 86, 0.5)',
+            'rgba(75, 192, 192, 0.5)',
+            'rgba(255, 99, 132, 0.5)',
+            'rgba(54, 162, 235, 0.5)',
+            'rgba(255, 206, 86, 0.5)',
+            'rgba(75, 192, 192, 0.5)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)'
+          ],
+          borderWidth: 1
         }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
       }
-    }
-  });
+    });
+  
 }
 
 $(document).ready(function () {
