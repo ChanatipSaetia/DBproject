9d51c287e3385c469aba196a860ec6bcf14c62b9
魂火SoulFire <chanatippung@gmail.com>
2017-04-30 18:37:53 +0700

    add mock course_pre

--- tools/dbseed/schema/course-prerequisite.js
+++ tools/dbseed/schema/course-prerequisite.js
@@ -6,8 +6,11 @@ class CoursePrerequisiteTable extends BaseTable {
   }
 
   generateMockData() {
+    const course_pre = [...require('../course_pre.json')];
+    for (let i = 0; i < course_pre.length; i++) {
+      super.putData(course_pre[i])
+    }
   }
 }
 
 module.exports = CoursePrerequisiteTable;
-
