35d8f054197cfe4add9a341aadbc40941cfefb91
zFord Nutthawat <zfordn@gmail.com>
2017-04-30 23:44:24 +0700

    หีแตด seed พังว้อย

--- tools/dbseed/schema/absent-record-has-student.js
+++ tools/dbseed/schema/absent-record-has-student.js
@@ -9,10 +9,9 @@ class AbsentRecordHasStudentTable extends BaseTable {
   generateMockData() {
     const studentData = super.getDepTable('student').getData();
     const absentData = super.getDepTable('absent_record').getData();
-    for (let i = 0; i < 200; i++) {
-      var absent = {}
+    for (const absentPiece of absentData) {
       var students = chance.pickset(studentData, chance.integer({ min: 1, max: 5 }));
-      var aridd = chance.pickone(absentData).arid;
+      var aridd = absentPiece.arid;
 
       for (const student of students) {
         super.putData({
