8d782d5069e184811f0ebef7664421e53804dd33
魂火SoulFire <chanatippung@gmail.com>
2017-05-01 07:33:22 +0700

    fix some enroll

--- public_src/js/enroll.js
+++ public_src/js/enroll.js
@@ -1,5 +1,13 @@
 $(function() {
+  let grade = $('#grade').DataTable({
+    "paging": false
+  });
+  // let detail = $('#detail').DataTable({
+  //   "paging": false
+  // });
+
   let ent = $('.nav-tabs').data('ent');
+
   function addRow(table, row, more) {
     if (row['grade'] == 'Z' && !more) {
       return table;
@@ -53,104 +61,105 @@ $(function() {
       table = addRow(table, row, false);
       detail_table = addRow(detail_table, row, true);
     }
+
+    grade.clear();
+
     $('.grade_table').html(table);
 
     $('.detail_table').html(detail_table);
 
-    let grade = $('#grade').DataTable({
-      "paging": false
-    });
-
-    let detail = $('#detail').DataTable({
-      "paging": false
-    });
-
   }
 
   let sid = $('#sid').html()
-  $('.tab_semester').click(function(){
+  $('.tab_semester').click(function() {
     let year = $(this).data('year')
     console.log(year);
     let semester = $(this).data('semester')
     $.ajax({
+        method: "POST",
+        url: "/enroll/detail",
+        data: {
+          sid: sid,
+          year: year,
+          semester: semester
+        }
+      })
+      .done(function(result) {
+        console.log(result);
+        DrawEnroll(result)
+      });
+    $.ajax({
+        method: "POST",
+        url: "/enroll/summary",
+        data: {
+          sid: sid,
+          year: year,
+          semester: semester,
+          ent_year: ent
+        }
+      })
+      .done(function(result) {
+        console.log(result);
+        drawSumTable(result)
+      });
+  })
+
+  $.ajax({
       method: "POST",
       url: "/enroll/detail",
       data: {
         sid: sid,
-        year: year,
-        semester: semester
+        year: ent,
+        semester: '1'
       }
     })
     .done(function(result) {
-        console.log(result);
-        DrawEnroll(result)
+      console.log(result);
+      DrawEnroll(result)
     });
-    $.ajax({
+
+
+  $.ajax({
       method: "POST",
       url: "/enroll/summary",
       data: {
         sid: sid,
-        year: year,
-        semester: semester,
+        year: ent,
+        semester: '1',
         ent_year: ent
       }
     })
     .done(function(result) {
-        console.log(result);
-        drawSumTable(result)
-    });
-  })
-
-  $.ajax({
-    method: "POST",
-    url: "/enroll/detail",
-    data: {
-      sid: sid,
-      year: ent,
-      semester: '1'
-    }
-  })
-  .done(function(result) {
-      console.log(result);
-      DrawEnroll(result)
-  });
-
-
-  $.ajax({
-    method: "POST",
-    url: "/enroll/summary",
-    data: {
-      sid: sid,
-      year: ent,
-      semester: '1',
-      ent_year: ent
-    }
-  })
-  .done(function(result) {
       console.log(result);
       drawSumTable(result)
-  });
+    });
 
 
-function drawSumTable(result) {
-  let sum_table = '<tr ';
-  if (result['gpax'] > 3.6) {
-    sum_table += 'class="success"' + '>';
-  } else if (result['gpax'] > 3.25) {
-    sum_table += 'class="info"' + '>';
-  } else if (result['gpax'] < 1.8) {
-    sum_table += 'class="danger"' + '>';
-  } else if (result['gpax'] < 2.0) {
-    sum_table += 'class="warning"' + '>';
-  } else {
-    sum_table += '>';
+  function drawSumTable(result) {
+    let sum_table = '<tr ';
+    if(result['gpax'] == 0){
+      sum_table += '>';
+    } else if (result['gpax'] > 3.6) {
+      sum_table += 'class="success"' + '>';
+    } else if (result['gpax'] > 3.25) {
+      sum_table += 'class="info"' + '>';
+    } else if (result['gpax'] < 1.8) {
+      sum_table += 'class="danger"' + '>';
+    } else if (result['gpax'] < 2.0) {
+      sum_table += 'class="warning"' + '>';
+    } else {
+      sum_table += '>';
+    }
+    for (var re in result) {
+      if(result['gpax'] == 0){
+        sum_table += '<td>-</td>';
+      } else {
+        sum_table += '<td>' + result[re] + '</td>';
+      }
+    }
+    sum_table += '</tr>';
+    $('.sum_grade_table').html(sum_table);
   }
-  for (var re in result) {
-    sum_table += '<td>' + result[re] + '</td>';
-  }
-  sum_table += '</tr>';
-  $('.sum_grade_table').html(sum_table);
-}
 
 
 });
